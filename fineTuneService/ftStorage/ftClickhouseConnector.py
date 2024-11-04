42b1e45626b35d585eabf003ad38d40ac1a1b3c1
datasyntez <broisekbmarvin@gmail.com>
2024-11-04 08:27:20 +0000

    reject db

--- fineTuneService/ftStorage/ftClickhouseConnector.py
+++ fineTuneService/ftStorage/ftClickhouseConnector.py
@@ -1,8 +1,6 @@
 from ftConfiguration.ftConfig import CH_DB_HOST, CH_DB_PORT
 from clickhouse_driver import Client
 import json
-import pandas as pd
-import numpy as np
 
 client = Client(host=CH_DB_HOST, port=CH_DB_PORT, settings={'use_numpy': True})
 
