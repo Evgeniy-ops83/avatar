f236ca5dc46d8d2a3342e04053058c2f037053d2
pych <subotinga@mail.ru>
2024-11-04 09:36:11 +0300

    flask app completions

--- fineTuneService/ftStorage/ftClickhouseConnector.py
+++ fineTuneService/ftStorage/ftClickhouseConnector.py
@@ -1,6 +1,8 @@
 from ftConfiguration.ftConfig import CH_DB_HOST, CH_DB_PORT
 from clickhouse_driver import Client
 import json
+import pandas as pd
+import numpy as np
 
 client = Client(host=CH_DB_HOST, port=CH_DB_PORT, settings={'use_numpy': True})
 
